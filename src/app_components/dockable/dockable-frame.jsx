74d763b265a31d859dda6bd809cb32408dd0c594
Daniel Black <daniel.l.black@usace.army.mil>
2023-10-18 13:49:09 -0700

    Update

--- src/app_components/dockable/dockable-frame.jsx
+++ src/app_components/dockable/dockable-frame.jsx
@@ -1,16 +1,27 @@
 import React, { Component } from "react";
 import _ from "lodash";
 import { Responsive, WidthProvider } from "react-grid-layout";
-import DockableItem from "./dockable-item";
 import ProjectTree from "../project-tree";
 import ComponentEditorComponent from "../component-editor-component";
 import MapComponent from "../map-component";
 import MessageComponent from "../message-component";
+import { generateLayout } from "./utils";
 
 const ResponsiveReactGridLayout = WidthProvider(Responsive);
 const COMPONENT = "component";
 
 class DockableFrame extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentBreakpoint: "lg",
+      compactType: "vertical",
+      resizeHandles: ["se"],
+      mounted: false,
+      layouts: { lg: generateLayout(["se"]) },
+    };
+  }
+
   componentsToDisplay = [
     { name: "Project", id: COMPONENT + "1", component: <ProjectTree /> },
     {
@@ -21,6 +32,40 @@ class DockableFrame extends Component {
     { name: "Map", id: COMPONENT + "3", component: <MapComponent /> },
     { name: "Messages", id: COMPONENT + "4", component: <MessageComponent /> },
   ];
+
+  onCompactTypeChange = () => {
+    const { compactType: oldCompactType } = this.state;
+    const compactType =
+      oldCompactType === "horizontal"
+        ? "vertical"
+        : oldCompactType === "vertical"
+        ? null
+        : "horizontal";
+    this.setState({ compactType });
+  };
+
+  onResizeTypeChange = () => {
+    const resizeHandles =
+      this.state.resizeHandles === availableHandles ? ["se"] : availableHandles;
+    this.setState({
+      resizeHandles,
+      layouts: { lg: generateLayout(resizeHandles) },
+    });
+  };
+
+  onLayoutChange = (layout, layouts) => {
+    this.props.onLayoutChange(layout, layouts);
+  };
+
+  onNewLayout = () => {
+    this.setState({
+      layouts: { lg: generateLayout(this.state.resizeHandles) },
+    });
+  };
+
+  onDrop = (layout, item, e) => {
+    alert(`Element parameters: ${JSON.stringify(item)}`);
+  };
 
   generateLayoutItems() {
     return this.componentsToDisplay.map((component) => ({
@@ -43,6 +88,12 @@ class DockableFrame extends Component {
           breakpoints={{ lg: 1200, md: 996, sm: 768, xs: 480, xxs: 0 }}
           cols={{ lg: 12, md: 10, sm: 6, xs: 4, xxs: 2 }}
           rowHeight={30}
+          onBreakpointChange={this.onBreakpointChange}
+          onDrop={this.onDrop}
+          measureBeforeMount={false}
+          useCSSTransforms={this.state.mounted}
+          compactType={this.state.compactType}
+          preventCollision={!this.state.compactType}
           layouts={{
             lg: layoutItems,
             md: layoutItems,
