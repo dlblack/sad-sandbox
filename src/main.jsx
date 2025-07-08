5c027db64603d09a8440d6cd1cf4ea0e09da2a46
Daniel Black <daniel.l.black@usace.army.mil>
2025-07-08 15:06:11 -0700

    Updated dependencies. Added README.

--- src/main.jsx
+++ src/main.jsx
@@ -4,8 +4,6 @@ import { StyleProvider } from "./styles/StyleContext";
 import App from "./App";
 import "./styles/css/bootstrap/bootstrap.cyborg.min.css";
 import "./styles/css/index.css";
-import "../node_modules/react-grid-layout/css/styles.css";
-import "../node_modules/react-resizable/css/styles.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
